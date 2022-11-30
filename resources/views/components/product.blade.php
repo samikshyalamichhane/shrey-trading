99dc0951ee63b5a08539f85b26958f86b0c1eff5
samikshyalamichhane <samikshya.lamichhane111@gmail.com>
2022-11-30 13:04:46 +0545

    update

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,9 +1,7 @@
 <ul class="plain-nav-tabs nav nav-tabs js-enable-tab-url" id="component-1" role="tablist">
-@if(auth()->guard('client')->user())
     <li class="nav-item">
         <a class="nav-link " data-toggle="tab" href="#component-1-1" role="tab" aria-controls="component-1-1" aria-selected="true"><strong>My Products</strong></a>
     </li>
-    @endif
     <li class="nav-item active">
         <a class="nav-link" data-toggle="tab" href="#component-1-2" role="tab" aria-controls="component-1-2" aria-selected="false"><strong>All Products</strong></a>
     </li>
